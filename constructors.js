3441f2c474dbf558876dcc7b34a7fc55a02709be
Jason Reeder <info@jasonreeder.com>
2017-03-01 08:49:11 -0800

    Add inheritance

--- constructors.js
+++ constructors.js
@@ -1,8 +1,22 @@
 'use strict';
 
-function Cat(name, owner) {
+function Animal(name, owner) {
   this.name = name;
   this.owner = owner;
+}
+
+Animal.prototype.eat = function () {
+  return "MMM food";
+};
+
+Function.prototype.inherits = function (ParentClass) {
+  this.prototype = Object.create(ParentClass.prototype);
+};
+
+Cat.inherits(Animal);
+
+function Cat(name, owner) {
+  Animal.call(this, name, owner);
 }
 
 Cat.prototype.cuteStatement = function () {
@@ -11,19 +25,21 @@ Cat.prototype.cuteStatement = function () {
 
 let amy = new Cat("Amy", "Bo");
 let yma = new Cat("Yma", "Jason");
+
 console.log(amy.cuteStatement());
+console.log(amy.eat());
 
 Cat.prototype.cuteStatement = function () {
   return `Everyone loves ${this.name}`;
 };
 
-console.log(amy.cuteStatement());
+// console.log(amy.cuteStatement());
 
 Cat.prototype.meow = function () {
   return "meeeeeeeeeeeowe";
 };
 
-console.log(amy.meow());
+// console.log(amy.meow());
 
 amy.meow = function () {
   return "roar!!";
@@ -31,10 +47,10 @@ amy.meow = function () {
 
 amy.name = "Amy the Tiger";
 
-console.log("Yma's meow");
-console.log(yma.meow());
+// console.log("Yma's meow");
+// console.log(yma.meow());
 
-console.log(`${amy.name}'s meow`);
-console.log(amy.meow());
+// console.log(`${amy.name}'s meow`);
+// console.log(amy.meow());
 
-console.log(amy.owner);
+// console.log(amy.owner);
