ce2690a36f6acedaa82ae5bcd27fd9931fd07626
Jason Reeder <info@jasonreeder.com>
2017-03-01 08:36:54 -0800

    Add methods to prototypes

--- array_exercises.js
+++ array_exercises.js
@@ -1,6 +1,19 @@
 /*jslint node: true */
 'use strict';
 
+Array.prototype.myUniq = function () {
+  let uniqueArray = [];
+
+  this.forEach( (el) => {
+    if (!uniqueArray.includes(el)) {
+      uniqueArray.push(el);
+    }
+  });
+
+  return uniqueArray;
+};
+
+
 function myUniq(array) {
   let result = [];
 
@@ -14,6 +27,7 @@ function myUniq(array) {
 }
 
 // console.log(myUniq([1, 2, 1, 3, 3]))
+// console.log([1, 2, 1, 3, 3].myUniq());
 
 function twoSum(array) {
   let result = [];
@@ -29,8 +43,23 @@ function twoSum(array) {
   return result;
 }
 
+Array.prototype.twoSum = function () {
+  let sumLocs = [];
+
+  for(let i = 0; i < this.length; i++) {
+    for(let j = i+1; j < this.length; j++) {
+      if (this[i] + this[j] === 0) {
+        sumLocs.push([i, j]);
+      }
+    }
+  }
+
+  return sumLocs;
+};
+
 
 // console.log(twoSum([-1, 0, 2, -2, 1]));
+// console.log([-1, 0, 2, -2, 1].twoSum());
 
 function myTranspose(array) {
   let result = [];
@@ -38,10 +67,9 @@ function myTranspose(array) {
   for (var i = 0; i < array.length; i++) {
     let row = [];
 
-    // TODO: The linter is mad. Is there a better way?
-    array.forEach((subArray) => {
-      row.push(subArray[i]);
-    });
+    for(let j = 0; j < array.length; j++) {
+      row.push(array[j][i]);
+    }
 
     result.push(row);
   }
@@ -49,9 +77,34 @@ function myTranspose(array) {
   return result;
 }
 
-console.log(myTranspose([
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8]
-  ])
-);
+Array.prototype.myTranspose = function () {
+  let transposed = [];
+
+  for(let i = 0; i < this.length; i++) {
+    let row = [];
+
+    for(let j = 0; j < this[i].length; j++) {
+      row.push(this[j][i]);
+    }
+
+    transposed.push(row);
+  }
+
+  return transposed;
+};
+
+
+// console.log(myTranspose([
+//     [0, 1, 2],
+//     [3, 4, 5],
+//     [6, 7, 8]
+//   ])
+// );
+
+let grid = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8]
+];
+
+console.log(grid.myTranspose());
